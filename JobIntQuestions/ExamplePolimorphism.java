5f21e0304433d13edb9d34e693172fbf56b06735
artart <artart537@gmail.com>
2020-03-25 07:26:00 -0700

    about polimorphizm

---
+++ JobIntQuestions/ExamplePolimorphism.java
@@ -0,0 +1,43 @@
+﻿Сильная зависимость в коде :
+class ClassicalMusic {
+  // код для доступа к классич музыке
+}
+
+class MusicPlayer {
+    privat ClassicalMusic classicalMusic;
+
+    public void playMusic() {
+        classicalMusic = new ClassicalMusic();
+
+ 
+         //..код для воспроизведения музыки
+}
+}
+
+
+Исп интерфейс для уменьшения зависимости : полиморфизм
+
+interface Music {
+  //.. код для доступа к любому жанру музыки
+}
+
+class ClassicalMusic implements Music {
+  //..код для доступа к классич музыке
+}
+
+class RockMusic implements Music {
+   //..код для доступа к рок музыке
+}
+
+class MusicPlayer {
+     private Music misic;
+
+     public void playMusic() {
+         music = new ClassicalMisuc();  //полиморфизм
+         // или
+         music = new RockMusic();    //полиморфизм 
+ 
+        //.. код для воспроизведения музыки
+
+   }
+}        

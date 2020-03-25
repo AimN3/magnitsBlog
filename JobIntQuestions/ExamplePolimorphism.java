2b0cd119d1018797c5e17654d60d401d24580cf5
artart <artart537@gmail.com>
2020-03-25 08:26:53 -0700

    polimorfizm_IoC

--- JobIntQuestions/ExamplePolimorphism.java
+++ JobIntQuestions/ExamplePolimorphism.java
@@ -31,7 +31,9 @@ class RockMusic implements Music {
 
 class MusicPlayer {
      private Music misic;
-
+     public MusicPlayer(Music music) {
+         this.music = music;
+     }
      public void playMusic() {
          music = new ClassicalMisuc();  //полиморфизм
          // или
@@ -41,3 +43,13 @@ class MusicPlayer {
 
    }
 }        
+
+ниже создаем объект который нужно будет внедрить в music player
+
+class UseMusicPlayer {
+      piblic static void main(String[] arg) {
+          MusicPlayer musicPlayer = new MusicPlayer(new ClassicalMusic());
+       //вначале созд объект new ClassicalMusic и затем он внедряется в конструктор
+         // при создании объекта new MusicalPlayer
+}
+}
